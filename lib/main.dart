7045fbd0fba5615b841df75e8a9b45c64467821e
Aurelio <aureliotjoa9@gmail.com>
2019-11-05 03:01:28 +0700

    upload master

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+import './HomePage.dart';
+
+void main(){
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primaryColor: Colors.white,
+    ),
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => new MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  String title = 'Humanity';
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+      seconds: 5,
+      navigateAfterSeconds: new HomePage(),
+      title: new Text(this.title,
+      style: new TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 20,
+        color: Colors.greenAccent
+        ),
+      ),
+      backgroundColor: Colors.white,
+      styleTextUnderTheLoader: new TextStyle(),
+      onClick: ()=> print('Wait a moment please'),
+      loaderColor: Colors.greenAccent,
+    );
+  }
+}
